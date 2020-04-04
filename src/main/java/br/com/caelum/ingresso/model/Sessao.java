f8be5bae5281c0120024eef11ea1f0e1ebbd815a
Wellington Silva <wsswellingtonsalvino@gmail.com>
2020-04-04 15:33:36 -0300

    add classe Sessao

--- src/main/java/br/com/caelum/ingresso/model/Sessao.java
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -2,11 +2,29 @@ package br.com.caelum.ingresso.model;
 
 import java.time.LocalTime;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
 public class Sessao {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private LocalTime horario;
+	
+	@ManyToOne
 	private Sala sala;
+	
+	@ManyToOne
 	private Filme filme;
+	
+	public Sessao() {
+		
+	}
 
 	public Sessao(LocalTime horario, Filme filme, Sala sala) {
 		this.horario = horario;
